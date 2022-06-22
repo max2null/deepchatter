3d1faf34d9d5f9e1782f5c2be952e08cf0db0e84
max <s>
2022-06-22 15:53:37 +0200

    init

---
+++ src/main/java/org/max/deepchatter/DeepchatterApplication.java
@@ -0,0 +1,13 @@
+package org.max.deepchatter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepchatterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeepchatterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/max/deepchatter/DeepchatterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.max.deepchatter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepchatterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
